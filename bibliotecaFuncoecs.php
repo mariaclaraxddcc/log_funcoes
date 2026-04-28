f06483fb376fb4c1abff2bb7e64677ffb5bf2d4b
mariaclaraxddcc <maria.c.santos29@edu.senai.br>
2026-04-28 12:00:07 -0300

    Add files via upload

--- bibliotecaFuncoecs.php
+++ bibliotecaFuncoecs.php
@@ -56,24 +56,50 @@ function areaCirculo ($raio, $pi ) {
 function areaTrapezio ($baseMaior, $baseMenor, $altura ) {
     return (($baseMaior + $baseMenor) * $altura) /2;
 }
+}
 
+
+//atvd3
+Namespace saude {
+    //$agua = 0.35;
+    //$frequencia = 220;
+
+function calcularmc ($peso, $altura) {
+    return $peso / $altura**2;
+}
+
+
+function valorIdealAgua ($peso) {
+    $agua = 0.35;
+    return $peso * $agua;
+}
+
+function frequenciaCardiacaMaxima ($idade) {
+    $frequencia = 220;
+    return $$frequencia = 220 - $idade;
 }
 
 
 
+function LibrasParaQuilo ($libras) {
+    $libras = 0.4536;
+    return $libras * 0.4536;
+}
 
 
+function CaloriasBasais ($peso, $idade, $sexo, $altura){
 
+if ($sexo = "masculino"){
+return 66.5 + (13.75 * $peso) + (5.003 * $altura) - (6.75 * $idade);
+}
+else {
+    ($sexo = "feminino");
+return 655.1 + (9.563 * $peso) + (1.850 * $altura) - (6.75 * $idade);
+}
 
+}
 
-
-
-
-
-
-
-
-
+}
 
 
 
--- entrada_dados.php
+++ entrada_dados.php
@@ -1,5 +1,5 @@
 <?php
-
+//atvd2
 require_once "bibliotecaFuncoecs.php";
 
 use function geometria\areaQuadrado;
@@ -18,9 +18,12 @@ use function geometria\areaTrapezio;
 echo "area trapezio: ", areaTrapezio (30, 20, 10 ),"\n";
 
 
+//atvd3
+require_once "bibliotecaFuncoecs.php";
+use function saude\calcularmc;
+echo "calcular mc: ", calcularmc (56, 1.56), "\n"; 
 
-
-
-
+use function saude\valorIdealAgua;
+echo "valor ideal de agua: ", valorIdealAgua (56, 0.35), "\n";
 
 ?>
